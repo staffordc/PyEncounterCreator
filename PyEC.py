ee1d57ffcdcbf7772e27c749183335a16331c3e1
Unknown <stafford.carh@gmail.com>
2019-02-21 17:09:47 -0500

    Beginnings of Filter

--- PyEC.py
+++ PyEC.py
@@ -1,5 +1,6 @@
 monsters = {
-    "goblin": ["50 XP", "AC 15", "HP 7 2d6", "Speed 30 ft", "pg# 166"]
+    "goblin": ["XP 50", "AC 15", "HP 7 (2d6)", "Speed 30 ft", "pg# 166"]
 }
-for key in monsters:
-    print(monsters["goblin"])
+for key, value in monsters.items():
+    if (key, value == "XP 50"):
+        print(key, value)
