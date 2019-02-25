e4e5eb8610d34d5a53b115d375db25c85667be27
Unknown <stafford.carh@gmail.com>
2019-02-25 12:20:45 -0500

    Added PrettyPrint to this for better output look

--- PyEC.py
+++ PyEC.py
@@ -1,9 +1,13 @@
+import pprint
+
+pp = pprint.PrettyPrinter(indent=0)
+
 monsters = {
     "goblin": [50, "AC 15", "HP 7 (2d6)", "Speed 30 ft", "pg# 166"],
     "aarakocra": [50, "AC 12", "HP 13 (3d8)", "Speed 20ft, fly 50 ft", "pg# 12"],
     "ankheg": [450, "AC 14, 11 while prone", "HP 39 (6d10+6", "Speed 30 ft, burrow 10ft", "pg# 21"],
     "azer": [450, "AC 17", "HP 39 (6d8+12)", "Speed 30 ft", "pg# 22"],
-    "banshee": [1,100, "AC 12", "HP 58 (13d8)", "Speed 0ft, fly 40 ft", "pg# 23"],
+    "banshee": [1, 100, "AC 12", "HP 58 (13d8)", "Speed 0ft, fly 40 ft", "pg# 23"],
     "basilisk": [700, "AC 15", "HP 52 (8d8 + 16), Speed 20 ft", "pg#24"],
     "spectator": [700, "AC 14", "HP 39 (6d8 + 12)", "Speed 0 ft, fly 30 ft", "pg# 30"],
     "needle blight": [50, "AC 12", "HP 11 (2d8 + 2)", "Speed 30 ft", "pg# 32"],
@@ -24,3 +28,6 @@ def player_input_math(intput, dictionary):
         if(intput in value and intput == value[0] or intput > value[0]):
             new_dictionary[key] = value
     return new_dictionary
+
+
+pp.pprint(player_input_math(player_input, monsters))
